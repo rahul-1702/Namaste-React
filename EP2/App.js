9e080b8eaa129f5dcd335eb7fe463fceed301eb4
Rahul-taak <rtaak786@gmail.com>
2025-05-04 22:54:49 +0530

    ep2 done

---
+++ EP2/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+    className: "parent-container",
+  },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1", key: 1 }, "I am H1 tag"),
+    React.createElement("h2", { id: "heading2", key: 2 }, "I am H2 tag"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
