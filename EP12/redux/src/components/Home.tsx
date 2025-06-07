0113e844b2de7f23860b1224a061193ecda690a0
Rahul-taak <rtaak786@gmail.com>
2025-06-07 23:43:48 +0530

    redux initial satge

---
+++ EP12/redux/src/components/Home.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState<string[]>([]);
+
+  useEffect(() => {
+    const FETCH_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    (async () => {
+      const response = await fetch(FETCH_URL);
+      const json = await response.json();
+      if (json) {
+        setData(json.restaurants);
+      }
+    })();
+  }, []);
+
+  return (
+    <div className="flex gap-10 flex-col">
+      <h1>Welcome Redux Food Store</h1>
+      <section>
+        <h2>All Restaurants</h2>
+        <ul>
+          {data &&
+            data.length > 0 &&
+            data.map((restaurant, index) => (
+              <li key={index} className="border p-2">
+                {restaurant}
+              </li>
+            ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
