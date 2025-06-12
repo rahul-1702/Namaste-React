2f2f46234d21ba86fb6dbd85f967a66ed08ce464
Rahul-taak <rtaak786@gmail.com>
2025-06-12 22:50:09 +0530

    use effect removed

--- EP12/redux/src/components/Home.tsx
+++ EP12/redux/src/components/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState<string[]>([]);
+  // const [data, setData] = useState<string[]>([]);
 
   useEffect(() => {
     const FETCH_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
