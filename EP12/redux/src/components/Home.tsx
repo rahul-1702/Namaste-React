a669c4646f697ed5a50b93c0d1df2b429349013c
Rahul-taak <rtaak786@gmail.com>
2025-06-16 23:19:00 +0530

    first commit

--- EP12/redux/src/components/Home.tsx
+++ EP12/redux/src/components/Home.tsx
@@ -1,35 +1,54 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const [data, setData] = useState<string[]>([]);
+  const [data, setData] = useState<any>();
+  const [restaurants, setRestaurants] = useState({});
 
   useEffect(() => {
-    const FETCH_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const FETCH_URL =
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
     (async () => {
       const response = await fetch(FETCH_URL);
       const json = await response.json();
-      // if (json) {
-      //   setData(json.restaurants);
-      // }
-      // console.log("json : ", json);
-    })();
+      if (json) {
 
+        setData(
+          json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants
+        );
+
+        setRestaurants({
+          name: data?.info?.name,
+          id: data?.info?.name,
+          rating: data?.info?.avgRating,
+          cuisines: data?.info?.cuisines,
+          isOpen: data?.info?.isOpen,
+          costForTwo: data?.info?.costForTwo,
+          deliveryTime: data?.info?.sla?.deliveryTime,
+          cloudinaryImageId: data?.info?.cloudinaryImgeId,
+        });
+      }
+    })();
   }, []);
+
+  useEffect(() => {
+    console.log("Data fetched:", restaurants);
+  }, [data, restaurants]);
 
   return (
     <div className="flex gap-10 flex-col">
       <h1>Welcome Redux Food Store</h1>
       <section>
         <h2>All Restaurants</h2>
-        <ul>
-          {/* {data &&
+        {/* <ul>
+          {data &&
             data.length > 0 &&
             data.map((restaurant, index) => (
-              <li key={index} className="border p-2">
-                {restaurant}
+              <li key={index} className="border p-2 text-white">
+              
               </li>
-            ))} */}
-        </ul>
+            ))}
+        </ul> */}
       </section>
     </div>
   );
