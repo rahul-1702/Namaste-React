c603b85f85209a21b4a50d17440f95f6c5b9379c
Rahul-taak <rtaak786@gmail.com>
2025-07-08 13:12:44 +0530

    testing module simple done

--- EP13/src/__tests__/ContactUs.test.js
+++ EP13/src/__tests__/ContactUs.test.js
@@ -1,13 +1,39 @@
 import ContactUs from "../components/ContactUs";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-test("Checks Header Component Loads or not", () => {
-  render(<ContactUs />);
+describe("Grouping Some Test Cases", () => {
+  it("Checks Header Component Loads or not", () => {
+    render(<ContactUs />);
+  });
+
+  it("Checks text 'Contact Us' present or not", () => {
+    const text = screen.getByText("Contact Us");
+
+    // Assertion
+    expect(text).toBeInTheDocument();
+  });
+
+  describe("Nested Grouping of Test Cases", () => {
+    test("Checks button present or not", () => {
+      const button = screen.getByRole("button");
+
+      // Assertion
+      expect(button).toBeInTheDocument();
+    });
+
+    test("Checks input elements present or not", () => {
+      // Querying
+      const inputs = screen.getAllByRole("textbox");
+
+      const length = inputs.length;
+
+      console.log("Inputs Length : ", length); // Returns React Element or JSX
+      console.log("Inputs : ", inputs); // Returns React Element or JSX
+
+      // Assertion
+      expect(length).toBe(4);
+      // expect(length).not.toBe(4);
+    });
+  });
 });
-
-// test("Checks text 'Redux - Food Store' present or not", () => {
-//   const text = screen.getByText("Redux - Food Store");
-
-//   // Assertion
-//   expect(text).toBeInTheDocument();
-// });
