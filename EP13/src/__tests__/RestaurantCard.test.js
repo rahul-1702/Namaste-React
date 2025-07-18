042900eeca3ba54afe3689556f12d54af7c0d508
Rahul-taak <rtaak786@gmail.com>
2025-07-18 22:35:09 +0530

    done testing here

--- EP13/src/__tests__/RestaurantCard.test.js
+++ EP13/src/__tests__/RestaurantCard.test.js
@@ -1,7 +1,44 @@
+import { afterEach, beforeEach, afterAll, beforeAll } from "node:test";
 import AllRestaurantCard from "../components/AllRestaurantCard";
 import MOCK_DATA from "../mocks/resCardMock.json";
+import { render } from "@testing-library/react";
+
+// for async operation use act function, like if component used fetch inside
+/*
+  it("Should render Restaurant Card Compoentn with Mock Data", async () => {
+    await act(async () => {
+      render(<AllRestaurantCard restaurant={MOCK_DATA} />);
+    })
+  }
+*/
+
+// create custom dom APIs like fetch
+/* 
+  global.fetch = jest.fn(() => {
+    Promise.resolve({
+      json: () => Promise.resolve(MOCK_DATA)
+    })
+  })
+*/
 
 it("Should render Restaurant Card Compoentn with Mock Data", () => {
+
+  beforeAll(() => {
+    console.log("Restaurant Card Before Each ...");
+  })
+
+  beforeEach(() => {
+    console.log("Restaurant Card Before Each ...");
+  })
+
+  afterAll(() => {
+    console.log("Restaurant Card Before Each ...");
+  })
+
+  afterEach(() => {
+    console.log("Restaurant Card Before Each ...");
+  })
+
   render(<AllRestaurantCard restaurant={MOCK_DATA} />);
 
   const name = screen.getByText("Pizza Hut");
